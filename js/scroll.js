b161d98ebc794298b763c3430fcccbce616356d1
Sonny Castillo <scastillo-2018547@kinal.edu.gt>
2020-08-20 16:54:08 -0600

    Javascript modificado

--- js/scroll.js
+++ js/scroll.js
@@ -1,17 +1,11 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+$(document).ready(function(){
+  var altura= $('.menu').offset().top;
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+  $(window).on('scroll', function(){
+    if ( $(window).scrollTop() > altura){
+      $('.menu').addClass('menu-fixed');
+    } else {
+      $ ('.menu').removeClass('menu-fixed');
+    }
+  });
+});
